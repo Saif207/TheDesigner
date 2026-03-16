b07a408083514a584e599e57774804e0edcf36e6
Saif Mohamed <khsaifkhwas@gmail.com>
2026-03-16 15:15:41 +0300

    added 1. Category filtering 2. Product detail page

--- TheDesigner/store/views.py
+++ TheDesigner/store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Product 
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
 from django.contrib.auth.models import User
@@ -53,3 +53,19 @@ def register_user(request):
             return redirect('register')
     else:
         return render(request,'register.html', {'form': form})
+    
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
+
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category': category})
+    except:
+        messages.error(request, "Category does not exist.")
+        return redirect('home')
+    
