16c202ff4fd3bfd204fb235122ed1fda9ab8b6d4
Saif Mohamed <khsaifkhwas@gmail.com>
2026-03-22 01:25:30 +0300

    Created a User Profile where the User can update his info and right now just his name and email

--- TheDesigner/store/views.py
+++ TheDesigner/store/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm,UpdateUserForm
 from django import forms
 
 
@@ -54,6 +54,24 @@ def register_user(request):
     else:
         return render(request,'register.html', {'form': form})
     
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request,"User Has been Updated")
+            return redirect("home")
+        return render(request,"update_user.html",{"user_form":user_form})
+    
+    else:
+        messages.success(request,"You Must be Logged In To Access That Page!!")
+        return redirect("home")
+
 
 def product(request, pk):
     product = Product.objects.get(id=pk)
