1ea4331e24d528247f3f06c10e868ebc1b3847b0
Saif Mohamed <khsaifkhwas@gmail.com>
2026-04-19 23:00:42 +0300

    Created a Generate Order, so the admin dont have to manually by himself (thats obvious). Now when ever the user place an Order it will say order is placed and then it will appear in the admin in Order Category.

--- TheDesigner/payment/models.py
+++ TheDesigner/payment/models.py
@@ -39,10 +39,10 @@ post_save.connect(create_shipping, sender=User)
 
 #Create Order Model
 class Order(models.Model):
-    #Foreign Key
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     full_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=250)
+    phone = models.CharField(max_length=20, blank=True)   # <-- add this line
     shipping_address = models.TextField(max_length=1000)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
--- TheDesigner/payment/views.py
+++ TheDesigner/payment/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
 from cart.cart import Cart
-from payment.forms import ShippingForm
-from payment.models import ShippingAddress
-from payment.forms import PaymentForm
+from payment.forms import ShippingForm,PaymentForm
+from payment.models import ShippingAddress,Order,OrderItem
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 def billing_info(request):
@@ -11,6 +11,10 @@ def billing_info(request):
         cart_products = cart.get_prods()
         quantities = cart.get_quants()
         totals = cart.cart_total()
+
+        #Create a session with shipping Info
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
 
         #check to see if user is logged in
         if request.user.is_authenticated:
@@ -44,3 +48,45 @@ def checkout(request):
         #Checkout as guest
         shipping_form = ShippingForm(request.POST or None)
         return render(request, "payment/checkout.html", {"cart_products":cart_products,"quantities":quantities,"totals":totals, "shipping_form":shipping_form})
+    
+
+def process_order(request):
+    if request.POST:
+
+        cart = Cart(request)
+        cart_products = cart.get_prods()
+        quantities = cart.get_quants()
+        totals = cart.cart_total()
+        
+        payment_form = PaymentForm(request.POST or None)
+        #Get Shipping session Data
+        my_shipping = request.session.get('my_shipping')
+
+
+        #Gather Order Info
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        phone = my_shipping['shipping_phone']
+        #Create shipping address from session Info
+        shipping_address = f"{my_shipping['shipping_block']}\n{my_shipping['shipping_street']}\n{my_shipping['shipping_house']}\n{my_shipping['shipping_area']}\n{my_shipping['shipping_governorate']}\n{my_shipping['shipping_country']}"
+        amount_paid = totals
+
+        #Create Order
+        if request.user.is_authenticated:
+            user = request.user
+            #Create Order
+            create_order = Order(user=user, full_name=full_name, email=email, phone=phone, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            messages.success(request,"Order Placed Successfully")
+            return redirect ("home")
+        else:
+            #Not Logged In
+            #Create Order
+            create_order = Order(full_name=full_name, email=email, phone=phone, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            messages.success(request,"Order Placed Successfully")
+            return redirect ("home")
+
+    else:
+        messages.success(request,"Access Denied")
+        return redirect ("home")
